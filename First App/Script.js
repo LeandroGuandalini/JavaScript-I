6c51d4ab7f9058b9c3553b29a2f392304da35d8a
LeandroGuandalini <leandro.gribeiro@al.infnet.edu.br>
2024-04-22 22:12:51 -0300

    Adicionando prompt e parseInt/Float

--- First App/Script.js
+++ First App/Script.js
@@ -1,12 +1,27 @@
+// constante 
+// const SALARIO_ATE_20 = 1000
+// const SALARIO_ACIMA_20 = 2000
+
 //input
-anoNascimento = 2000
-nome = "Leandro"
+nome = prompt('Qual o seu nome?')
+anoNascimento = parseInt(prompt('Que ano você nasceu?'))
+salarioBase = parseFloat(prompt('Qual o seu salário base?'))
+gratificacao = parseFloat(prompt('Qual sua gratificação?'))
+bonus = parseFloat(prompt('Qual seu bônus?'))
+desconto = parseFloat(prompt('Quanto tem de desconto?'))
+
+salarioLiquido = 0
+// adicional = 0
 
 //processamento
 hoje = new Date()
 anoAtual = hoje.getFullYear()
 idade = anoAtual - anoNascimento
-mensagem = "Eu sou o " + nome + " e tenho " + idade + " anos"
+salarioLiquido = salarioBase + gratificacao + bonus - desconto + adicional
+
+// if
+
 
 //output
+mensagem = `Eu sou o ${nome}, tenho ${idade} anos e ganho R$${salarioLiquido}`
 alert(mensagem)
