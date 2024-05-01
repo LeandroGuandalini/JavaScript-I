3898048596683a42837596b4081ce3ba6d47d813
Leandro Guandalini <leandro.gribeiro@al.infnet.edu.br>
2024-04-30 23:09:36 -0300

    Revert "implementando forms em html"
    This reverts commit 546792ffbc8b007db1a3b258a49bc216cd899869.

--- First App/Script.js
+++ First App/Script.js
@@ -1,30 +1,27 @@
-function calcularIdade(oAnoNascimento){
-  const hoje = new Date()
-  const anoAtual = hoje.getFullYear()
-  const idade = anoAtual - oAnoNascimento
-  return idade
-}
+// constante 
+// const SALARIO_ATE_20 = 1000
+// const SALARIO_ACIMA_20 = 2000
 
-function calcularValorAdicional(aIdade){
-  const SALARIO_ATE_20 = 1000
-  const SALARIO_ACIMA_20 = 2000
-  const idadeLimite = 20
-  let adicional = aIdade <= idadeLimite ? SALARIO_ATE_20 : SALARIO_ACIMA_20
-  return adicional 
-}
+//input
+nome = prompt('Qual o seu nome?')
+anoNascimento = parseInt(prompt('Que ano você nasceu?'))
+salarioBase = parseFloat(prompt('Qual o seu salário base?'))
+gratificacao = parseFloat(prompt('Qual sua gratificação?'))
+bonus = parseFloat(prompt('Qual seu bônus?'))
+desconto = parseFloat(prompt('Quanto tem de desconto?'))
 
-function impressao(){
-  const nome = document.getElementById("nome").value
-  const anoNascimento = parseInt(document.getElementById("anoNascimento").value)
-  const salarioBase = parseFloat(document.getElementById("salarioBase").value)
-  const gratificacao = parseFloat(document.getElementById("gratificacao").value)
-  const bonus = parseFloat(document.getElementById("bonus").value)
-  const desconto = parseFloat(document.getElementById("desconto").value)
+salarioLiquido = 0
+// adicional = 0
 
-  const idade = calcularIdade(anoNascimento)
-  let adicional = calcularValorAdicional(idade)
-  let salarioLiquido = salarioBase + gratificacao + bonus - desconto + adicional
+//processamento
+hoje = new Date()
+anoAtual = hoje.getFullYear()
+idade = anoAtual - anoNascimento
+salarioLiquido = salarioBase + gratificacao + bonus - desconto
 
-  let mensagem = `Eu sou o ${nome}, tenho ${idade} anos e ganho R$${salarioLiquido}`
-  alert(mensagem)
-}
+// if
+
+
+//output
+mensagem = `Eu sou o ${nome}, tenho ${idade} anos e ganho R$${salarioLiquido}`
+alert(mensagem)
