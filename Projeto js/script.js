36616c7e42b4f8a9216ab50b30f7925ae287d12b
Leandro Guandalini <leandro.gribeiro@al.infnet.edu.br>
2024-06-04 22:17:57 -0300

    Update script.js
    Arrumando funções e introduzindo objeto

--- Projeto js/script.js
+++ Projeto js/script.js
@@ -1,8 +1,16 @@
-function registrarHistorico(mensagem){
+function registrarHistorico(mensagem, sucesso){
   const historicoAlteracao = document.getElementById("historicoAlteracao");
+  const divAlertContainer = document.getElementById("alertContainer");
+  // divAlertContainer.style.display = "none";
+  if (sucesso) {
   const li = document.createElement("li");
   li.textContent = mensagem;
   historicoAlteracao.appendChild(li);
+  } else {
+  // divAlertContainer.style.display = "block";
+  divAlertContainer.className = "alert alert-info"
+  divAlertContainer.innerHTML = `<strong>Atenção!</strong> ${mensagem}.`
+  }
 }
 
 function validarCamposObrigatorios(produto, quantidade, precoUnitario, dataTransacao){
@@ -25,11 +33,7 @@ function gerarNumeroTransacao () {
   return Math.floor(Math.random() * 10000000);
 }
   
-function registrarVenda(){
-  let produto = "Arroz";
-  let quantidade = 5;
-  let precoUnitario = 10.5;
-  
+function registrarVenda(produto, quantidade, precoUnitario){
   let dataTransacao = registrarDataTransacao();
   let numeroTransacao = gerarNumeroTransacao ();
 
@@ -40,24 +44,36 @@ function registrarVenda(){
   if(validaCamposObrigatorios){
     if(validaQuantidadeProdutos){
       if(validaPrecoUnitario){
-        return `[${numeroTransacao}] Transação registrada! ${produto}, Qtde: ${quantidade}, Preço: R$${precoUnitario}, Data: ${dataTransacao}`;
+        return {
+          mensagem: `[${numeroTransacao}] Transação registrada! ${produto}, Qtde: ${quantidade}, Preço: R$${precoUnitario}, Data: ${dataTransacao}`,
+          sucesso: true
+        };
       } else {
-        return "Problemas na Validação de Preço Unitário!";
+        return {
+          mensagem: "Problemas na Validação de Preço Unitário!",
+          sucesso: false
+        };
       }
     } else {
-      return "Problemas na Validação de Quantidade de Produtos!";
+      return {
+        mensagem: "Problemas na Validação de Quantidade de Produtos!",
+        sucesso: false
+      };
     }
   } else {
-      return "Problemas na validação de campos obrigatórios!";
+      return {
+        mensagem: "Problemas na validação de campos obrigatórios!",
+        sucesso: false
+      };
   }
 }
 
 function vender (){
   let produto = document.getElementById("produto").value;
-  let quantidade = document.getElementById("quantidade").value;
-  let preco = document.getElementById("preco").value;
+  let quantidade = Number(document.getElementById("quantidade").value);
+  let preco = Number(document.getElementById("preco").value);
 
-  let mensagem = registrarVenda(produto, quantidade, preco)
+  let objeto = registrarVenda(produto, quantidade, preco)
 
-  registrarHistorico(mensagem);
+  registrarHistorico(objeto.mensagem, objeto.sucesso);
 }
